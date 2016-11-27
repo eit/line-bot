903d4432d8e4cf9f69ea1d973d4a05e5b03fbc13
Hsin <shinsonh@gmail.com>
2016-11-26 21:37:14 -0500

    fix typo

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	CHANNEL_ID: process.env.CHANNEL_ID,
-	CHANNEL_SECRECT: process.env.CHANNEL_SERECT,
+	CHANNEL_SECRECT: process.env.CHANNEL_SECRECT,
 	MID: ' ',
 	CHANNEL_TOKEN: process.env.CHANNEL_TOKEN
 }
